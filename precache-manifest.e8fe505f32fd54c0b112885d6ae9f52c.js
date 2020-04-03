ce71d565e5b53c7a091c3a314bfdfcd9677a46f7
mmplanic <mmplanic@gmail.com>
2020-04-03 12:07:51 +0200

    Updates

---
+++ precache-manifest.e8fe505f32fd54c0b112885d6ae9f52c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f270e831839a9c2ac0c3b3efc74242ad",
+    "url": "/bootcamp-final/index.html"
+  },
+  {
+    "revision": "958a597b3ffcf99ba2c2",
+    "url": "/bootcamp-final/static/css/main.71f2284c.chunk.css"
+  },
+  {
+    "revision": "f9811d3db9101524d373",
+    "url": "/bootcamp-final/static/js/2.425a7ae2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bootcamp-final/static/js/2.425a7ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958a597b3ffcf99ba2c2",
+    "url": "/bootcamp-final/static/js/main.2f6071dc.chunk.js"
+  },
+  {
+    "revision": "a2172d469a3a217cd050",
+    "url": "/bootcamp-final/static/js/runtime-main.bf37f301.js"
+  },
+  {
+    "revision": "d0fc638fed533be968b181e3c4de2810",
+    "url": "/bootcamp-final/static/media/ExtraSales.d0fc638f.ttf"
+  },
+  {
+    "revision": "be066ad9cf260d492b26b3f002f25d5a",
+    "url": "/bootcamp-final/static/media/ExtraSalesBlank.be066ad9.ttf"
+  },
+  {
+    "revision": "12ca2fc6ec9464d4dd1c381f5f7be65c",
+    "url": "/bootcamp-final/static/media/SUPERHEROES WORLD.12ca2fc6.png"
+  },
+  {
+    "revision": "a3216ac4445364e59cfc6218184ceb89",
+    "url": "/bootcamp-final/static/media/background.a3216ac4.png"
+  },
+  {
+    "revision": "c4096c0774f923fd68d2ecb019d5f55a",
+    "url": "/bootcamp-final/static/media/fightDesign.c4096c07.png"
+  },
+  {
+    "revision": "0765598f3c618c26806631046d7a9197",
+    "url": "/bootcamp-final/static/media/header.0765598f.png"
+  },
+  {
+    "revision": "3c1af9e9c056b338159decf28c9a32e2",
+    "url": "/bootcamp-final/static/media/hot-grill-demo.3c1af9e9.ttf"
+  },
+  {
+    "revision": "c5240c89d6993ea807887742902a9fdd",
+    "url": "/bootcamp-final/static/media/select1.c5240c89.png"
+  },
+  {
+    "revision": "71647f7aa30f5b5bc91c5a3a0c730914",
+    "url": "/bootcamp-final/static/media/select2.71647f7a.png"
+  },
+  {
+    "revision": "72a2381792e4193c1c8b12c9d77aa3ca",
+    "url": "/bootcamp-final/static/media/select3.72a23817.png"
+  },
+  {
+    "revision": "d0170a752b8498f853c0576a99b3dbf1",
+    "url": "/bootcamp-final/static/media/select4.d0170a75.png"
+  }
+]);
